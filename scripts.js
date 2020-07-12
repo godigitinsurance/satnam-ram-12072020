d62bc8cf397839bbbe82aaaf2339e61a6db6b503
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-12 17:47:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Satnam Ram");
+$("#policyNumber").text("VM46209480");
+$("#startDate").text("12-Jul-2020");
+$("#endDate").text("11-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("VM46209480");
+$("#make").text("BAJAJ");
+$("#model").text("DISCOVER DTS-SI");
+$("#vehicalSubType").text("DRUM BRAKE ES");
+$("#engineNo").text("JBMBTC47528");
+$("#chassis").text("MD2DSPAZ2TWC28368");
